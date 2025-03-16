e55fce87551582ccc34a7e36b31aff31f5a8e1b7
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2025-03-16 18:20:54 +0530

    fix: Update Vercel configuration to streamline build command and adjust Vite base path

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Add this line
+  base: './', // Set this for correct pathing
 });
+
